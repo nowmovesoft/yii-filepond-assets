86eafb85faccb83889b130a533fc5d27bffa6600
Michael Naumov <vommuan@gmail.com>
2020-04-10 10:42:56 +0700

    All supported frameworks and plugins were added in composer.json

---
+++ src/FilePondAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace nms\filepond;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset for base FilePond implementation.
+ * @author Michael Naumov <vommuan@gmail.com>
+ */
+class FilePondAsset extends AssetBundle
+{
+
+}
