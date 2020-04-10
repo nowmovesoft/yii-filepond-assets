a209084747e9879660a3d2c2579ba5f2bcb819ff
Michael Naumov <vommuan@gmail.com>
2020-04-10 17:13:36 +0700

    FileTypeValidationAsset

---
+++ src/FileTypeValidationAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace nms\filepond;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset for File type validation plugin in FilePond.
+ * @author Michael Naumov <vommuan@gmail.com>
+ */
+class FileTypeValidationAsset extends AssetBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@npm/filepond-plugin-file-validate-type';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $js = [
+        (YII_ENV_DEV) ? 'dist/filepond-plugin-file-validate-type.js' : 'dist/filepond-plugin-file-validate-type.min.js',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $depends = [
+        'nms\filepond\FilePondAsset',
+    ];
+}
