96ddedc30961ba473c54c20ea78b9b7764f532c3
Michael Naumov <vommuan@gmail.com>
2020-04-10 11:12:36 +0700

    Asset for FilePond base implementation

--- src/FilePondAsset.php
+++ src/FilePondAsset.php
@@ -10,5 +10,22 @@ use yii\web\AssetBundle;
  */
 class FilePondAsset extends AssetBundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@npm/filepond';
 
+    /**
+     * {@inheritdoc}
+     */
+    public $css = [
+        (YII_ENV_DEV) ? 'dist/filepond.css' : 'dist/filepond.min.css',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $js = [
+        (YII_ENV_DEV) ? 'dist/filepond.js' : 'dist/filepond.min.js',
+    ];
 }
