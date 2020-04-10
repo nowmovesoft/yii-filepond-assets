c5c1155d7da5fb431f9b7819a2aeac59a1ab3eb8
Michael Naumov <vommuan@gmail.com>
2020-04-10 16:54:43 +0700

    FileRenameAsset

---
+++ src/FileRenameAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace nms\filepond;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset for File metadata plugin in FilePond.
+ * @author Michael Naumov <vommuan@gmail.com>
+ */
+class FileRenameAsset extends AssetBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@npm/filepond-plugin-file-rename';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $js = [
+        (YII_ENV_DEV) ? 'dist/filepond-plugin-file-rename.js' : 'dist/filepond-plugin-file-rename.min.js',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $depends = [
+        'nms\filepond\FilePondAsset',
+    ];
+}
