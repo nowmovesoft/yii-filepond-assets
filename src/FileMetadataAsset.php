c4b815ab77d496b4894a974bb499c10689b8e8a1
Michael Naumov <vommuan@gmail.com>
2020-04-10 16:44:55 +0700

    FileMetadataAsset

--- src/FileMetadataAsset.php
+++ src/FileMetadataAsset.php
@@ -5,21 +5,21 @@ namespace nms\filepond;
 use yii\web\AssetBundle;
 
 /**
- * Asset for File encode plugin in FilePond.
+ * Asset for File metadata plugin in FilePond.
  * @author Michael Naumov <vommuan@gmail.com>
  */
-class FileEncodeAsset extends AssetBundle
+class FileMetadataAsset extends AssetBundle
 {
     /**
      * {@inheritdoc}
      */
-    public $sourcePath = '@npm/filepond-plugin-file-encode';
+    public $sourcePath = '@npm/filepond-plugin-file-metadata';
 
     /**
      * {@inheritdoc}
      */
     public $js = [
-        (YII_ENV_DEV) ? 'dist/filepond-plugin-file-encode.js' : 'dist/filepond-plugin-file-encode.min.js',
+        (YII_ENV_DEV) ? 'dist/filepond-plugin-file-metadata.js' : 'dist/filepond-plugin-file-metadata.min.js',
     ];
 
     /**
